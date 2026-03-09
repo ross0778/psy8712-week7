85ea9236dbe6555a6e47c2afcbc77842232f943c
ross0778 <ross0778@umn.edu>
2026-03-09 16:50:56 -0500

    Working on visualizations

--- R/week7.R
+++ R/week7.R
@@ -2,7 +2,7 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(lubridate)
-#library calls
+library(GGally)
 #library calls
 
 # Data Import and Cleaning
@@ -15,3 +15,27 @@ week7_tbl <- read_csv("../data/week3.csv") %>%
   filter(q6 == 1) %>% 
   select(-q6) %>% 
   mutate(timeSpent = as.numeric(difftime(timeEnd, timeStart, units = "mins")))
+
+
+
+# Visualization
+ggpairs(week7_tbl %>% 
+          select(starts_with("q")))
+
+week7_tbl %>% 
+  ggplot(aes(x = timeStart, y = q1)) +
+  geom_point() + 
+  labs(x = "Date of Experiment", y = "Q1 Score")
+
+
+week7_tbl %>% 
+  ggplot(aes(x = q1, y = q2, color = gender)) +
+  geom_jitter() +
+  labs(x = "q1", y = "q2", color = "Participant Gender")
+
+week7_tbl %>% 
+  ggplot(aes(x = q1, y = q2)) +
+  geom_jitter() +
+  facet_wrap(~ gender) +
+  labs(x = "Score on Q1", y = "Score on Q2")
+
