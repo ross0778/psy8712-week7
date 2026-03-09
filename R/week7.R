69a4908328595975f006f7a6c992060e067f4156
ross0778 <ross0778@umn.edu>
2026-03-09 14:26:26 -0500

    Set up new project

---
+++ R/week7.R
@@ -0,0 +1,7 @@
+# Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+#library calls
+#library calls
+#library calls
+#library calls
+# Data Import and Cleaning
